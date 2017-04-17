640635b58651433e38ca1f23530d9080ec0f3040
from0tohero <liuchang@bu.edu>
2017-04-17 14:26:25 -0400

    Create Chapter_18_Either_Monad.hs

---
+++ Chapter_18_Either_Monad.hs
@@ -0,0 +1,22 @@
+{--
+Chapter 18 Monad
+Short Exercise: Either Monad
+Implement the Either Monad
+--}
+
+data Sum a b = First a | Second b deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second $ f b
+
+instance Applicative (Sum a) where
+  pure x = Second x
+  First e <*> _ = First e
+  _ <*> First e = First e
+  Second f <*> Second x = Second $ f x
+
+instance Monad (Sum a) where
+  return = pure
+  First e >>= _  = First e
+  Second x >>= f = f x
