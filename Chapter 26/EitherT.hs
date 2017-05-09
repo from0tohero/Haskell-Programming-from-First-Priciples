f908faf2689dfb6d55020f28679f34088b43fbd5
from0tohero <liuchang@bu.edu>
2017-05-09 19:42:31 -0400

    Update EitherT.hs

--- Chapter 26/EitherT.hs
+++ Chapter 26/EitherT.hs
@@ -15,3 +15,19 @@ instance Monad m => Monad (EitherT e m) where
         case ea of
             Left e -> return (Left e)
             Right x -> runEitherT $ f x
+
+-- 4. Write the swapEitherT helper function for EitherT.
+swapEither :: Either e a -> Either a e
+swapEither (Left e) = Right e
+swapEither (Right a) = Left a
+
+swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT ema) = EitherT $ fmap swapEither ema
+
+-- 5. Write the transformer variant of the either catamorphism.
+eitherT :: Monad m => (a -> m c) -> (b -> m c) -> EitherT a m b -> m c
+eitherT f g (EitherT amb) = do
+    ab <- amb
+    case ab of 
+        Left x -> f x
+        Right y -> g y
